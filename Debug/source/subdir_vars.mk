d75dcd901efb498b9014b929e1404ea6e4f8ce7a
Hardik Senjaliya <hardyksenjaliya243@gmail.com>
2019-05-01 17:44:42 -0600

    adding project structure

---
+++ Debug/source/subdir_vars.mk
@@ -0,0 +1,50 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../source/logger_task.c \
+../source/main.c \
+../source/motor_task.c \
+../source/pressure_sensor_task.c \
+../source/ultrasonic_task.c 
+
+C_DEPS += \
+./source/logger_task.d \
+./source/main.d \
+./source/motor_task.d \
+./source/pressure_sensor_task.d \
+./source/ultrasonic_task.d 
+
+OBJS += \
+./source/logger_task.obj \
+./source/main.obj \
+./source/motor_task.obj \
+./source/pressure_sensor_task.obj \
+./source/ultrasonic_task.obj 
+
+OBJS__QUOTED += \
+"source\logger_task.obj" \
+"source\main.obj" \
+"source\motor_task.obj" \
+"source\pressure_sensor_task.obj" \
+"source\ultrasonic_task.obj" 
+
+C_DEPS__QUOTED += \
+"source\logger_task.d" \
+"source\main.d" \
+"source\motor_task.d" \
+"source\pressure_sensor_task.d" \
+"source\ultrasonic_task.d" 
+
+C_SRCS__QUOTED += \
+"../source/logger_task.c" \
+"../source/main.c" \
+"../source/motor_task.c" \
+"../source/pressure_sensor_task.c" \
+"../source/ultrasonic_task.c" 
+
+
---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,32 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../freertos_demo_ccs.cmd 
+
+LIB_SRCS += \
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/ccs/Debug/driverlib.lib 
+
+C_SRCS += \
+../startup_ccs.c 
+
+C_DEPS += \
+./startup_ccs.d 
+
+OBJS += \
+./startup_ccs.obj 
+
+OBJS__QUOTED += \
+"startup_ccs.obj" 
+
+C_DEPS__QUOTED += \
+"startup_ccs.d" 
+
+C_SRCS__QUOTED += \
+"../startup_ccs.c" 
+
+
